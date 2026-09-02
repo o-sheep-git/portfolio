21835da98c37b03d7928bb9507120df0358a9c74
おひつじ <281835703+o-sheep-git@users.noreply.github.com>
2026-09-02 13:31:22 +0900

    GTM導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
-import {
-  GoogleAnalytics,
-  GoogleTagManager,
-  GoogleTagManagerNoScript,
-} from "@/components/analytics";
 import { site } from "@/lib/site";
 import "./globals.css";
 
@@ -30,13 +25,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KPLHVZVR');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${notoSansJP.className} flex min-h-screen flex-col antialiased`}>
-        <GoogleTagManagerNoScript />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KPLHVZVR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SiteHeader />
         <main className="flex-1">{children}</main>
         <SiteFooter />
-        <GoogleTagManager />
-        <GoogleAnalytics />
       </body>
     </html>
   );
